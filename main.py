fa57cd29559011c8a3e0bb227cd42d259b797083
Truong Minh Phat <tmphat1312@gmail.com>
2024-04-08 14:12:18 +0700

    chore: move down the function

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ from scipy.spatial import ConvexHull
 from queue import Queue
 from queue import PriorityQueue
 
-def bresenham(x0, y0, x1, y1):
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = -1 if x0 > x1 else 1
-    sy = -1 if y0 > y1 else 1
-    err = dx - dy
-    # points = [(x0, y0)]
-    points = []
-    while x0 != x1 or y0 != y1:
-        e2 = err * 2
-        if e2 > -dy:
-            err -= dy
-            x0 += sx
-        if e2 < dx:
-            err += dx
-            y0 += sy
-        points.append((x0, y0))
-    points.pop(len(points) - 1)
-    return points
-
 GROUP_MEMBERS = [
     "21120502 - Tran Duc Minh",
     "21120515 - Tran Phuoc Nhan",
@@ -66,6 +46,25 @@ root.geometry("+0+0")
 root.resizable(False, False)
 root.configure(bg="white")
 
+def bresenham(x0, y0, x1, y1):
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = -1 if x0 > x1 else 1
+    sy = -1 if y0 > y1 else 1
+    err = dx - dy
+    # points = [(x0, y0)]
+    points = []
+    while x0 != x1 or y0 != y1:
+        e2 = err * 2
+        if e2 > -dy:
+            err -= dy
+            x0 += sx
+        if e2 < dx:
+            err += dx
+            y0 += sy
+        points.append((x0, y0))
+    points.pop(len(points) - 1)
+    return points
 
 class SearchBoard:
     def __init__(self, canvas) -> None:
